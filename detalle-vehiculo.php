25c528f402192ccbbefb7d15545547153fefa0af
DavidTapia1509 <davidtapiayepes@gmail.com>
2022-02-11 02:43:24 +0100

    subida

--- detalle-vehiculo.php
+++ detalle-vehiculo.php
@@ -35,13 +35,190 @@ $vehiculo = $sentencia->fetch(PDO::FETCH_OBJ);
         </div>
         <div class="col-md-6">
           <h1> <?php echo $vehiculo->nomVehiculo; ?> </h1>
-          <h6>S/. <?php echo $vehiculo->precio; ?></h6>
+          <h6>Desde: <?php echo $vehiculo->precio; ?>€</h6>
           <p><?php echo $vehiculo->detalle; ?></p>
         </div>
       </div>
     </div>
     <!-- fin detalle -->
+  <div class="container"> 
+      <img src="img/renaultclio.png"  style="width: 285px; height: 185px;" class="rounded float-start" alt="">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <img src="img/renaultclio3.png"  style="width: 285px; height: 185px;" class="rounded float-end" alt="">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <img src="img/renaultclio2.png"  style="width: 285px; height: 185px;" class="rounded float-end" alt="">
+  <div class="card-body">
+    <h5 class="card-title">Renault Clio 1.5DCI</h5>
+    <img src="img/icons8-us-dollar-circled.gif" style="width: 50px; height: 50px;" /> 
+    <p class="card-text"> <small class="text-primary">20.090€</small></p>
+  </div>
+  </div>
+  <!-- Tabla Renault Diesel-->  
+  <div class="container mt-5">
+    
+  
+ <table class="specifications-car-table table-secondary col-md-12">
+                <thead>
+                <tr>
+                    <th colspan="2 ">Prestaciones</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Velocidad máxima</td>
+                    <td>178 km/h </td>
+                </tr>
+                <tr>
+                    <td>Aceleración 0-100 km/h</td>
+                    <td> 14,7 s </td>
+                </tr>
+                <tr>
+                    <td>Consumo (urbano/extraurbano/mixto)</td>
+                    <td> 4,3/3,2/3,6 l/100 km </td>
+                </tr>
+                <tr>
+                    <td>CO2</td>
+                    <td> 95 </td>
+                </tr>
+                </tbody>
+                <thead>
+                <tr>
+                    <th colspan="2">Dimensiones</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Carrocería</td>
+                    <td> 5 puertas </td>
+                </tr>
+                <tr>
+                    <td>Largo/Ancho/Alto</td>
+                    <td> 4.050 /
+                         1.988 /
+                         1.440 mm </td>
+                </tr>
+                                                   <tr>
+                        <td>Peso</td>
+                        <td>1.277 kg</td>
+                    </tr>
+                                <tr>
+                <tr>
+                    <td>Carga máxima</td>
+                    <td> 1.702 kg </td>
+                </tr>
+                <tr>
+                    <td>Depósito</td>
+                    <td> 39 litros </td>
+                </tr>
+                <tr>
+                    <td>Maletero min</td>
+                    <td> 366 litros </td>
+                </tr>
+                <tr>
+                    <td>Maletero max</td>
+                    <td> 1.069 litros </td>
+                </tr>
+                <tr>
+                    <td>Plazas</td>
+                    <td> 5 </td>
+                </tr>
+                </tbody>
+                <thead>
+                <tr>
+                    <th colspan="2">Motorización</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <td>Número de cilindros</td>
+                    <td> 4 </td>
+                </tr>
+                <tr>
+                    <td>Disposición</td>
+                    <td> línea </td>
+                </tr>
+                <tr>
+                    <td>Combustible</td>
+                    <td> Diésel </td>
+                </tr>
+                <tr>
+                    <td>Cilindrada</td>
+                    <td> 1.461 cc </td>
+                </tr>
+                                <tr>
+                    <td>Potencia combustión</td>
+                    <td> 85 cv /
+                         3.750 rpm </td>
+                </tr>
+                                                                <tr>
+                    <td>Par motor</td>
+                    <td> 220 Nm /
+                         1.750 rpm </td>
+                </tr>
+                <tr>
+                    <td>Potencia específica</td>
+                    <td> 17,2 cv/l </td>
+                </tr>
+             <thead>
+                <tr>
+                    <th colspan="2">Transmisión</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Tracción</td>
+                    <td> Delantera </td>
+                </tr>
+                <tr>
+                    <td>Cambio</td>
+                    <td> Manual </td>
+                </tr>
+                <tr>
+                    <td>Número de velocidades</td>
+                    <td> 6 </td>
+                </tr>
+                </tbody>
+                <thead>
+                <tr>
+                    <th colspan="2">Chasis</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Suspensión delantera</td>
+                    <td> McPherson </td>
+                </tr>
+                <tr>
+                    <td>Suspensión trasera</td>
+                    <td> Eje rígido </td>
+                </tr>
+                <tr>
+                    <td>Frenos delanteros</td>
+                    <td> discos ventilados  </td>
+                </tr>
+                <tr>
+                    <td>Frenos traseros</td>
+                    <td> discos macizos  </td>
+                </tr>
+                <tr>
+                    <td>Dirección (tipo/asistencia)</td>
+                    <td> Eléctrica de asistencia variable </td>
+                </tr>
+                <tr>
+                    <td>Neumáticos delanteros Ancho/Perfil/Llanta</td>
+                    <td> 185 /
+                         65 /
+                         15  </td>
+                </tr>
+                <tr>
+                    <td>Neumáticos traseros Ancho/Perfil/Llanta</td>
+                    <td> 185 /
+                         65 /
+                         15  </td>
+                </tr>
+            </table>
+            </div>
 
+          
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
