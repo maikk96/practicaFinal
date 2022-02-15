20ded205b3f8853f35ce133bbcdb4f653a20184c
Miguel Sánchez <miguelhorneros@gmail.com>
2022-02-15 11:03:56 +0100

    Prueba2

--- login_conexion.php
+++ login_conexion.php
@@ -5,30 +5,23 @@
     }
 
     $error = '';
-    
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        
         $usuario = $_POST['usuario'];
         $clave = $_POST['clave'];
         $clave = hash('sha512', $clave);
-        
         try{
             $conexion = new PDO('mysql:host=localhost;dbname=concesionario', 'root', '');
             }catch(PDOException $prueba_error){
                 echo "Error: " . $prueba_error->getMessage();
             }
-        
         $statement = $conexion->prepare('
         SELECT * FROM login WHERE usuario = :usuario AND clave = :clave'
         );
-        
         $statement->execute(array(
             ':usuario' => $usuario,
             ':clave' => $clave
         ));
-            
         $resultado = $statement->fetch();
-        
         if ($resultado !== false){
             $_SESSION['usuario'] = $usuario;
             header('location: principal.php');
@@ -36,5 +29,4 @@
             $error .= '<i>Este usuario no existe</i>';
         }
     }
-    
 require 'login.php';
