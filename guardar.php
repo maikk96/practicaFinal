1b8a9ece6b240c372846305333b9915c21bcdab3
Miguel Sánchez <miguelhorneros@gmail.com>
2022-02-16 19:03:07 +0100

    registro

---
+++ guardar.php
@@ -0,0 +1,36 @@
+<?php
+include 'model/conexion.php';
+include 'funcs.php';
+
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+$usuario = $_POST['usuario'];
+$telefono = $_POST['telefono'];
+$correo = $_POST['correo'];
+$contraseña = $_POST['contraseña'];
+
+$hash = hashPassword($contraseña);
+
+$stmt = $bd->query("INSERT INTO usuarios VALUES ('','$nombre', '$apellidos', '$usuario', '$correo', '$telefono', '$contraseña')");
+
+
+if($stmt->execute()){
+  echo "Datos guardados correctamente";
+}else {
+  return "No se han guardado los datos";
+}
+
+if($stmt)
+{
+session_start();
+$_SESSION["usuario"] = $usuario;
+$_SESSION["nombre"] = $nombre;
+
+header("location:account.php?q=1");
+}
+else
+{
+header("location:index.php?q7=El usuario ya está registrado");
+}
+ob_end_flush();
+?>
