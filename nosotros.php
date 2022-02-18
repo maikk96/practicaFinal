70c17140943b14794e506c933640611b9dddf0c9
DavidTapia1509 <davidtapiayepes@gmail.com>
2022-02-18 17:49:32 +0100

    subida de Nosotros

--- nosotros.php
+++ nosotros.php
@@ -29,6 +29,38 @@ if (!isset($_SESSION['usuario'])) {
   
 
  <!-- fin vehiculos -->
+
+ <div class="container">
+<div style="text-align: center;">
+<h1>¿Quiénes somos?</h1>
+<div style="text-align: justify;"><br style="font-family: Consolas;">
+  <span style="font-family: Consolas;">Somos una empresa que se dedica a
+la compra-venta de vehiculos, de momento estamos activos en España,
+vendiendo vehiculos localmente. En un futuro haremos exportaciones del
+extrangero para que puedas tener tu flamante vehiculo nuevo dando igual
+la procedencia del vehiculo. </span><br style="font-family: Consolas;">
+<br style="font-family: Consolas;">
+  <span style="font-family: Consolas;">En cuanto a nuestros servicios
+vendemos vehiculos nuevos, tambien hacemos tasaciones de su vehiculo
+usado.</span><br style="font-family: Consolas;">
+<br style="font-family: Consolas;">
+<ul style="font-family: Consolas;">
+  <li><span style="font-weight: bold;">Vehiculos nuevos</span>: Como
+hemos mencionado, vendemos coches nuevos a precios asequibles. Nostros
+nos ocuparemosde enviale un pdf para que firme las condiciones via
+online o si quiere tambien tenemos la opcion de hacer la venta en una
+de uestras oficinas situadas en Madrid (España).</li>
+  <li><span style="font-weight: bold;">Tasaciones:</span> Se puede
+tasar su coche para que sepa el precio de valor de venta que se le
+ofreceria. Nosotros recibiremos la informacion mediante el relleno de
+un formulario web. <br>
+  </li>
+</ul>
+<br>
+</div>
+</div>
+</div>
+ 
   <!-- inicio footer -->
   <?php include 'maqueta/general.php' ?>
   <?php include 'maqueta/footer.php' ?>
